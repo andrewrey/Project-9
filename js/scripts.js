35c19c25d0bfd33a331f30230fa6c7a873e0b8cb
Andrew Reynolds <andrew@nearablecorp.com>
2020-04-18 20:57:43 -0700

    connected button to animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-button = document.querySelector('.menuButton');
+const button = document.querySelector('.menuButton');
+const menuOverlay = document.querySelector('.menuOverlay'); 
 let menuOpen = false;
+let menuOverlayAnimation = false;
+
 
 
 button.addEventListener('click', ()=>{
+  if (!menuOverlay.classList.contains('hidden')){
+    menuOverlay.classList.add('hidden');
+  } else {
+    menuOverlay.classList.remove('hidden');
+  }
+
+  if (!menuOverlayAnimation){
+    menuOverlay.classList.add('menuOverlayAnimation');
+    menuOverlayAnimation = true;
+  } else {
+    menuOverlay.classList.remove('menuOverlayAnimation');
+  }
   if(!menuOpen){
     button.classList.add('open');
     menuOpen = true
