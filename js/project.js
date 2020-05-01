12ef471e9e0730981675ba269c6c92e977b0c83d
Andrew Reynolds <andrew@nearablecorp.com>
2020-04-30 22:18:02 -0700

    added project object to build projects elemetns

---
+++ js/project.js
@@ -0,0 +1,20 @@
+//// Project Object /////
+
+
+const projects = {
+  project1: {
+    name: "My Personal Profile Page",
+    link: "",
+    image: "../img/project1.png",
+  },
+  project2: {
+    name: "Mobile-first Responsive Layout",
+    link: "",
+    image: "../img/project2.png",
+  },
+  project3: {
+    name: "An Online Registratoin Form",
+    link: "",
+    image: "../img/project3.png",
+  }
+}
