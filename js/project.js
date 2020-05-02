e79300402fccd70c6cef45815ff61eab5899aa26
Andrew Reynolds <andrew@nearablecorp.com>
2020-05-01 22:30:57 -0700

    completed project desritions up till project 6

--- js/project.js
+++ js/project.js
@@ -6,16 +6,58 @@ const projects = {
     name: "My Personal Profile Page",
     link: "",
     image: "img/projectImg/project1.png",
+    description: "Treehouse Project 1: Modified code for a basic webpage. Added Photos, text, styling, and video background.",
+    plateform: 'TeamTreeHouse'
   },
   project2: {
     name: "Mobile-first Responsive Layout",
     link: "",
     image: "img/projectImg/project2.png",
+    description: "Built a responsive, mobile-first layout using HTML and CSS. The responsive design adjusts to accommodate small, medium and large screens using min-width media queries.",
+    plateform: 'TeamTreeHouse'
+
   },
   project3: {
     name: "An Online Registratoin Form",
     link: "",
     image: "img/projectImg/project3.png",
+    description:"Built a responsive, mobile-friendly resgistration form using HTML form input types and attributes. This form uses a mobile-first approach with media queries allowing for both desktop and mobile viewing.",
+    plateform: 'TeamTreeHouse'
+  },
+  project4: {
+    name: "Web Style Guide",
+    link: "",
+    image: "img/projectImg/project4.png",
+    description:"TeamTreeHouse provided a completed page which needed it's CSS to be convereted into SASS. Completed this by breaking the SASS into several SASS partial files. Took advantage of using SASS variables and Mixins",
+    plateform: 'TeamTreeHouse'
+  },
+  project5: {
+    name: "An Interactive Photo Gallery",
+    link: "",
+    image: "img/projectImg/project5.png",
+    description:"Built an interactive photo gallery using JavaSCript and jQuery. Had to find a suitable lightbox jQuery plugin for this project that allowed for images to be displayed in an overlay. The page incorporates a photo search bar at the top that filters shown images based on text matching photos.",
+    plateform: 'TeamTreeHouse'
+  },
+  project6: {
+    name: "",
+    link: "",
+    image: "img/projectImg/project6.png",
+    description:"",
+    plateform: 'TeamTreeHouse'
+  },
+  project7: {
+    name: "",
+    link: "",
+    image: "img/projectImg/project7.png",
+    description:"",
+    plateform: 'TeamTreeHouse'
+  },
+  project8: {
+    name: "",
+    link: "",
+    image: "img/projectImg/project8.png",
+    description:"",
+    plateform: 'TeamTreeHouse'
   }
 }
 
