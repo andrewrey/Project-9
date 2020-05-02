3545f46ee3b5b2a3978838dc6ef599d615108a6d
Andrew Reynolds <andrew@nearablecorp.com>
2020-05-02 11:47:03 -0700

    completed descriptions for projects object

--- js/project.js
+++ js/project.js
@@ -7,14 +7,22 @@ const projects = {
     link: "",
     image: "img/projectImg/project1.png",
     description: "Treehouse Project 1: Modified code for a basic webpage. Added Photos, text, styling, and video background.",
-    plateform: 'TeamTreeHouse'
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png"
+    }
   },
   project2: {
     name: "Mobile-first Responsive Layout",
     link: "",
     image: "img/projectImg/project2.png",
     description: "Built a responsive, mobile-first layout using HTML and CSS. The responsive design adjusts to accommodate small, medium and large screens using min-width media queries.",
-    plateform: 'TeamTreeHouse'
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png"
+    }
 
   },
   project3: {
@@ -22,42 +30,75 @@ const projects = {
     link: "",
     image: "img/projectImg/project3.png",
     description:"Built a responsive, mobile-friendly resgistration form using HTML form input types and attributes. This form uses a mobile-first approach with media queries allowing for both desktop and mobile viewing.",
-    plateform: 'TeamTreeHouse'
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png"
+    }
   },
   project4: {
     name: "Web Style Guide",
     link: "",
     image: "img/projectImg/project4.png",
     description:"TeamTreeHouse provided a completed page which needed it's CSS to be convereted into SASS. Completed this by breaking the SASS into several SASS partial files. Took advantage of using SASS variables and Mixins",
-    plateform: 'TeamTreeHouse'
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png",
+      sass: "img/techImg/icons8-sass-48.png"
+    }
   },
   project5: {
     name: "An Interactive Photo Gallery",
     link: "",
     image: "img/projectImg/project5.png",
     description:"Built an interactive photo gallery using JavaSCript and jQuery. Had to find a suitable lightbox jQuery plugin for this project that allowed for images to be displayed in an overlay. The page incorporates a photo search bar at the top that filters shown images based on text matching photos.",
-    plateform: 'TeamTreeHouse'
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png",
+      sass: "img/techImg/icons8-sass-48.png",
+      jquery: "img/techImg/icons8-jquery-50.png"
+    }
   },
   project6: {
-    name: "",
+    name: "Game Show App",
     link: "",
     image: "img/projectImg/project6.png",
-    description:"",
-    plateform: 'TeamTreeHouse'
+    description:"Completed a 'Wheel of Success' game using Javascript to provide the game functionality. Treehouse provided basic HTML and CSS from which students were to build the game from. ",
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png",
+      sass: "img/techImg/icons8-sass-48.png",
+      js: "img/techImg/icons8-javascript-logo-50.png"
+    }
   },
   project7: {
-    name: "",
+    name: "WebApp Dashboard",
     link: "",
     image: "img/projectImg/project7.png",
-    description:"",
-    plateform: 'TeamTreeHouse'
+    description:"Created a mock employee dashboard using HTML, CSS and Javascript. In this project, worked with SVG images and Chart.js JavaScript Library for building the graphs on the page. Utilized local storage in order to remember user settings.     ",
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png",
+      sass: "img/techImg/icons8-sass-48.png",
+      js: "img/techImg/icons8-javascript-logo-50.png"
+    }
   },
   project8: {
-    name: "",
+    name: "API created Employee Directory",
     link: "",
     image: "img/projectImg/project8.png",
-    description:"",
-    plateform: 'TeamTreeHouse'
+    description:"Focus of this project was on learing how to use a third-party API and incorporate the data. Used Async Function and Fetch API to build a mock employee Directory with the functionality to Search for employees. ",
+    plateform: 'TeamTreeHouse',
+    technology: {
+      html: "img/techImg/icons8-html-5-48.png",
+      css: "img/techImg/icons8-css3-48.png",
+      sass: "img/techImg/icons8-sass-48.png",
+      js: "img/techImg/icons8-javascript-logo-50.png"
+    }
   }
 }
 
