0b2e496a97dab7c7c2c3f7afd817c2da3283a276
Andrew Reynolds <andrew@nearablecorp.com>
2020-05-03 21:02:59 -0700

    updated layout so cover screen is fixed but project page is absolute, updated sizing of project images from screens under 600px

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,42 @@ const pages = document.querySelectorAll('.page');
 const projectWrap = document.querySelector('.projectWrap');
 
 
+// ================== //
+// Animation JS Start //
+// ================== //
+
+const icon = document.querySelector('.animationWrap');
+const triTopLeft = document.querySelector('.topleft');
+const triTopRight = document.querySelector('.topright');
+const triBottomLeft = document.querySelector('.bottomleft');
+const triBottomRight = document.querySelector('.bottomright');
+const triangles = document.querySelectorAll('.tri');
+
+
+
+
+icon.addEventListener('click', ()=>{
+  triTopLeft.classList.add('tlAnimation');
+  triTopRight.classList.add('trAnimation');
+  triBottomLeft.classList.add('blAnimation');
+  triBottomRight.classList.add('brAnimation');
+  setTimeout(()=>{
+    triangles.forEach(tri =>{
+      if(tri.classList.contains('tlAnimation')){
+        tri.classList.remove('tlAnimation');
+      } else if (tri.classList.contains('trAnimation')) {
+        tri.classList.remove('trAnimation');
+      } else if (tri.classList.contains('blAnimation')){
+        tri.classList.remove('blAnimation');
+      } else if (tri.classList.contains('brAnimation')){
+        tri.classList.remove('brAnimation');
+      }
+    }) 
+  },3000);
+});
+
+
+
 
 /// Event Listener for switching header page from welcome to projects ///
 downArrow.addEventListener('click', ()=>{
